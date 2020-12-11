f229aea4c210fd8f2e81b1453d4f4bcbfdc86201
Anmol Sinha <anmolsinha1201@outlook.com>
2020-12-11 19:51:16 +0200

    Added adding a report for morning

---
+++ services/reportMorningService.js
@@ -0,0 +1,44 @@
+import { executeQuery } from "../database/database.js";
+import { required, minNumber, numberBetween, isNumber, isDate, validate } from "../deps.js"
+
+const getHello = async() => {
+	const res = await executeQuery("SELECT * FROM news");
+	if (res && res.rowCount > 0) {
+		return res.rowsOfObjects();
+	}
+
+	return [];
+}
+
+const getHelloSpecific = async(id) => {
+	const res = await executeQuery("SELECT * FROM news where id = $1", id);
+	if (res && res.rowCount > 0) {
+		return res.rowsOfObjects()[0];
+	}
+
+	return [];
+}
+
+const setHello = async(title, content) => {
+	await executeQuery("INSERT INTO news (title, content) VALUES ($1, $2);", title, content);
+}
+
+const deleteHello = async(id) => {
+	await executeQuery("delete from news where id = $1;", id);
+}
+
+export const addReport = async(data) => {
+	const rules = {
+		date : [required, isDate],
+		sleepDuration : [required, isNumber, minNumber(0)],
+		sleepQuality : [required, isNumber, numberBetween(1, 5)],
+		genericMood : [required, isNumber, numberBetween(1, 5)]
+	}
+
+	const [passes, errors] = await validate(data, rules);
+	if (!passes)
+		return errors;
+}
+
+
+export { getHello, setHello, getHelloSpecific, deleteHello };
