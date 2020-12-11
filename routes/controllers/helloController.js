a38132ee3cc66414d92887b03bac040e49d76dfe
Anmol Sinha <anmolsinha1201@outlook.com>
2020-12-11 19:52:08 +0200

    Added reporting more

--- routes/controllers/helloController.js
+++ routes/controllers/helloController.js
@@ -1,4 +1,5 @@
 import * as helloService from "../../services/helloService.js";
+import * as reportMorningService from "../../services/reportMorningService.js";
 
 const hello = async({render}) => {
 	render('index.ejs', { newsList: await helloService.getHello() });
@@ -12,12 +13,28 @@ export const landing = async({render}) => {
 	render('landing.ejs');
 };
 
-export const reportMorning = async({render}) => {
-	render('reportMorning.ejs');
+export const reportMorningGET = async({render}) => {
+	render('reportMorning.ejs', { errors : undefined, data : undefined });
 };
 
-export const reportEvening = async({render}) => {
-	render('reportEvening.ejs');
+export const reportMorningPOST = async({render, request}) => {
+	const body = request.body();
+	const document = await body.value;
+	
+	const data = {
+		date : document.get('date'),
+		sleepDuration : Number(document.get('sleepDuration')),
+		sleepQuality : Number(document.get('sleepQuality')),
+		genericMood : Number(document.get('genericMood'))
+	}
+
+
+	const errors = await reportMorningService.addReport(data);
+	render('reportMorning.ejs', { errors : errors, data : data });
+};
+
+export const reportEveningGET = async({render}) => {
+	render('reportEvening.ejs', { errors : undefined, data : undefined });
 };
  
 export { hello, helloSpecific };
