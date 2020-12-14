053c45df513b8aa3db96fc3c57e90e0430ffa8a5
Anmol Sinha <anmolsinha1201@outlook.com>
2020-12-14 20:59:02 +0200

    Added connection pool

--- database/database.js
+++ database/database.js
@@ -1,19 +1,19 @@
-import { Client } from "../deps.js";
+import { Client, Pool } from "../deps.js";
 import { config } from "../config/config.js";
 
-const getClient = () => {
-	return new Client(config.database);
-}
+const connectionPool = new Pool(config.database, 3);
+// const getClient = () => {
+// 	return new Client(config.database);
+// }
 
 const executeQuery = async(query, ...args) => {
-	const client = getClient();
+	const client = await connectionPool.connect();
 	try {
-		await client.connect();
 		return await client.query(query, ...args);
 	} catch (e) {
 		console.log(e);
 	} finally {
-		await client.end();
+		await client.release();
 	}
 }
 
